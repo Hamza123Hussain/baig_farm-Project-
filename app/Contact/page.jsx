5590fce9f505c1e74838477caa0c3b683cd521d7
Hamza123Hussain <hamzahussain14.hh@gmail.com>
2024-04-28 20:31:04 +0500

    ..

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image' // Import Next.js Image component
+import Link from 'next/link'
 
 const ContactForm = () => {
   return (
-    <div className=" flex  items-center justify-center min-h-screen  sm:items-center p-5">
+    <div className="  min-h-screen   p-5">
       <div className="mx-auto sm:px-6 lg:px-8">
         <div className="mt-8 overflow-hidden">
-          <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className=" flex flex-col justify-center items-center">
             <div className="p-6 mr-2  sm:rounded-lg">
               <h1 className="text-4xl sm:text-5xl text-black  font-extrabold tracking-tight">
                 Get in touch
@@ -19,9 +20,14 @@ const ContactForm = () => {
                   width={32}
                   height={32}
                 />
-                <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                  Acme Inc, Street, State, Postal Code
-                </div>
+                <Link
+                  href={'https://maps.app.goo.gl/NED3crVmmfnw11jD7?g_st=iw '}
+                >
+                  <div className=" ml-4 text-md tracking-wide font-semibold ">
+                    2-Km from Gaggar Chowk Towards Kotli Rai Abu Bakr, Kasur,
+                    Punjab 55050
+                  </div>
+                </Link>
               </div>
 
               <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
@@ -32,70 +38,11 @@ const ContactForm = () => {
                   height={32}
                 />
                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                  +44 1234567890
-                </div>
-              </div>
-
-              <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                <Image
-                  src="/email.svg" // Path to your email SVG icon
-                  alt="Email Icon"
-                  width={32}
-                  height={32}
-                />
-                <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                  info@acme.org
+                  +92 324 7868008
                 </div>
               </div>
             </div>
-
-            <form className="p-6 flex flex-col justify-center">
-              <div className="flex flex-col">
-                <label htmlFor="name" className="hidden">
-                  Full Name
-                </label>
-                <input
-                  type="name"
-                  name="name"
-                  id="name"
-                  placeholder="Full Name"
-                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                />
-              </div>
-
-              <div className="flex flex-col mt-2">
-                <label htmlFor="email" className="hidden">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Email"
-                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                />
-              </div>
-
-              <div className="flex flex-col mt-2">
-                <label htmlFor="tel" className="hidden">
-                  Number
-                </label>
-                <input
-                  type="tel"
-                  name="tel"
-                  id="tel"
-                  placeholder="Telephone Number"
-                  className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-indigo-500 focus:outline-none"
-                />
-              </div>
-
-              <button
-                type="submit"
-                className="md:w-32 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-indigo-500 transition ease-in-out duration-300"
-              >
-                Submit
-              </button>
-            </form>
+            <div>map here</div>
           </div>
         </div>
       </div>
